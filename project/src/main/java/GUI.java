bf5c899816207d1f6bd6866f470d1c7aa1587020
FSFitzgerald <63276291+FSFitzgerald@users.noreply.github.com>
2020-07-05 19:02:06 +0700

    Sửa điểm cho sinh viên - End here

--- project/src/main/java/GUI.java
+++ project/src/main/java/GUI.java
@@ -603,7 +603,76 @@ public class GUI extends JFrame{
                 }
             });
             diemSua = new JMenuItem("Sủa điểm");
-            diemSua.addActionListener(hl);
+            diemSua.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    panel.removeAll();
+                    panel1.removeAll();
+                    for(int i = 0; i < getComponentCount(); i++){
+                        if(getComponent(i) == panel1){
+                            remove(panel1);
+                        }
+                    }
+                    pack();
+                    panel.setLayout(new GridLayout(10, 2));
+                    JLabel mssv = new JLabel("Mã số sinh viên");
+                    JTextField mssvF = new JTextField();
+                    JLabel bm = new JLabel("Mã bộ môn");
+                    JTextField bmF = new JTextField();
+                    JLabel lop = new JLabel("Mã lớp");
+                    JTextField lopF = new JTextField();
+                    JLabel hvt = new JLabel("Họ và tên");
+                    JTextField hvtF = new JTextField();
+                    JLabel gk = new JLabel("Điểm GK");
+                    JTextField gkF = new JTextField();
+                    JLabel ck = new JLabel("Điểm CK");
+                    JTextField ckF = new JTextField();
+                    JLabel khac = new JLabel("Điểm khác");
+                    JTextField khacF = new JTextField();
+                    JLabel tong = new JLabel("Điểm tổng");
+                    JTextField tongF = new JTextField();
+                    JLabel mes = new JLabel();
+                    JButton commit = new JButton("Thay đổi");
+                    commit.addActionListener(new ActionListener() {
+                        @Override
+                        public void actionPerformed(ActionEvent e) {
+                            if (mssvF.getText().equals("") || bmF.getText().equals("") || lopF.getText().equals("") || hvtF.getText().equals("") || gkF.getText().equals("") || ckF.getText().equals("") || khacF.getText().equals("") || tongF.getText().equals("")){
+                                mes.setText("Không để trống");
+                            }else{
+                                BangDiem bd = new BangDiem(mssvF.getText(), bmF.getText(), lopF.getText(), hvtF.getText(), Float.parseFloat(gkF.getText()), Float.parseFloat(ckF.getText()), Float.parseFloat(khacF.getText()), Float.parseFloat(tongF.getText()));
+                                boolean sucss = BangDiemDAO.capNhatDiem(bd);
+                                if(sucss == false){
+                                    mes.setText("Sửa thất bại");
+                                }else{
+                                    remove(panel);
+                                    pack();
+                                    setSize(600, 600);
+                                }
+                            }
+                        }
+                    });
+                    panel.add(mssv);
+                    panel.add(mssvF);
+                    panel.add(bm);
+                    panel.add(bmF);
+                    panel.add(lop);
+                    panel.add(lopF);
+                    panel.add(hvt);
+                    panel.add(hvtF);
+                    panel.add(gk);
+                    panel.add(gkF);
+                    panel.add(ck);
+                    panel.add(ckF);
+                    panel.add(khac);
+                    panel.add(khacF);
+                    panel.add(tong);
+                    panel.add(tongF);
+                    panel.add(mes);
+                    panel.add(commit);
+                    add(panel);
+                    pack();
+                }
+            });
             diemSo.add(diemImport);
             diemSo.add(diemXem);
             diemSo.add(diemSua);
